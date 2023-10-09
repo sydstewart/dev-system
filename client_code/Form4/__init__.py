fb8dec56be02be9c3523e7e3eddb4497f016e6d6
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-10-09 15:34:25 +0000

    Edited Form4

--- client_code/Form4/__init__.py
+++ client_code/Form4/__init__.py
@@ -9,8 +9,28 @@ class Form4(Form4Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.map_1.center=GoogleMap.LatLng(54.1973265832562, -2.834603077700183)
+    self.map_1.zoom = 18
+    green_icon = GoogleMap.Icon(
+      url="http://maps.google.com/mapfiles/kml/paddle/grn-blank.png"
+    )
 
-    # Any code you write here will run before the form opens.
+    marker = GoogleMap.Marker(
+    animation=GoogleMap.Animation.DROP,
+    position=GoogleMap.LatLng(54.1973265832562, -2.834603077700183),
+    icon = green_icon
+    )
+     
+
+    self.map_1.add_component(marker)
+
+    def marker_click(sender, **event_args):
+       i =GoogleMap.InfoWindow(content=Label(text="This is Home!"))
+       i.open(map, sender)
+
+    marker.add_event_handler("click", marker_click)
+
+
 
   def drop_down_1_change(self, **event_args):
     """This method is called when an item is selected"""
@@ -24,4 +44,19 @@ class Form4(Form4Template):
       open_form('Form4')
     elif self.drop_down_1.selected_value == 'Form5':
       open_form('Form5')
+
+  # def map_1_click(self, lat_lng, pixel, **event_args):
+  #   """This method is called when the user clicks on the map."""
+    
+ 
+    # def marker_click(sender, **event_args):
+    #       i =GoogleMap.InfoWindow(content=Label(text="This is Cambridge!"))
+    #       i.open(map, sender)
+    # marker.add_event_handler("click", marker_click)
+
+
+
+
+
+
 
