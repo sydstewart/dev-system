2a2833342f9132fea9efb912dc3e8304a9984aaa
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-10-14 08:56:11 +0000

    Edited Form7

--- client_code/Form7/__init__.py
+++ client_code/Form7/__init__.py
@@ -24,45 +24,19 @@ class Form7(Form7Template):
     
     self.marker = mapboxgl.Marker({'red': '#944840', 'draggable': True})
     self.marker.setLngLat([-2.834603077700183, 54.1973265832562]).addTo(self.mapbox)
-    
-    self.geocoder = MapboxGeocoder({'accessToken': self.token ,
-    'marker': False}) #we've already added a marker 
-    
-    self.mapbox.addControl(self.geocoder)
 
     self.geocoder = MapboxGeocoder({'accessToken': mapboxgl.accessToken,
                                     'marker': False})
     self.mapbox.addControl(self.geocoder)
   
     self.geocoder.on('result', self.move_marker)
-    self.marker.on('drag', self.marker_dragged)
-  
-  
-  
-  
-  
-  
-  
-  
-  # def onDragEnd(self, result):
-  #     lnglat = result['result']['geometry']['coordinates']
-  #     self.marker.setLngLat(lnglat)
-  #     alert(lnglat)
-  #     # self.marker.on('dragend', onDragEnd);
-  # def onDragEnd(self, result):
-  #   lngLat = marker.getLngLat()
-  #   coordinates.style.display = 'block'
-  #   coordinates.innerHTML = f"Longitude: {lngLat.lng}<br />Latitude: {lngLat.lat}"
+    # self.marker.on('drag', self.marker_dragged)
 
-  #   marker.on('dragend', onDragEnd)
-#    def move_marker(self, result):
-#         #get the [longitude, latitude] coordinates from the JS object returned from 'result' 
-#         lnglat = result['result']['geometry']['coordinates']
-#         self.marker.setLngLat(lnglat)
-#         print(result)    
-# # mapboxgl.accessToken = self.token
-#     self.mapbox = mapboxgl.Map({'container': self.dom,
-#                                 'style': 'mapbox://styles/mapbox/streets-v11',
-#                                 # 'style': 'mapbox://styles/brookemyers/cklk04z7x1f5d17pedafupa3e',
-#                                 'center': [-2.834603077700183, 54.1973265832562], #center on Cambridge
-#                                 'zoom': 14})
+    
+    
+  def move_marker(self, result):
+    lnglat = result['result']['geometry']['coordinates']
+    self.marker.setLngLat(lnglat)
+    alert(lnglat)
+
+
