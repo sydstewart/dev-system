4b6660cede66b7941cff9df60573127b5e88947a
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-10-21 09:51:06 +0000

    Edited Form7

--- client_code/Form7/__init__.py
+++ client_code/Form7/__init__.py
@@ -13,29 +13,39 @@ class Form7(Form7Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    #token for mapbox
     self.token = "pk.eyJ1Ijoic3lkbmV5c3Rld2FydCIsImEiOiJjbG5vajJ1b3kwMG9xMnRscWcxa3p4YXBtIn0.mnKGtTfkOCDAGkdxA-cZnw"
     # Any code you write here will run before the form opens.
+    #set up dom for javascript for self.map_1
     self.dom = anvil.js.get_dom_node(self.map_1)
     # self.repeating_panel_1.items = app_tables.trees.search()
 
-    
+  #show form with maps
   def form_show(self, **event_args):
     """This method is called when the HTML panel is shown on the screen"""
+    
+    # get tree list into dropdowns
     treelist =  applications =list({(r['Name']) for r in app_tables.trees.search(tables.order_by('Name'))})
     self.drop_down_1.items =treelist
     self.drop_down_2.items = treelist
+    
+    #declare  mapbox token
     mapboxgl.accessToken = self.token
+
+    # layout map using self.maxbox
     self.mapbox = mapboxgl.Map({
     'container': self.dom,
-    
-    # // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+     # // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     'style': 'mapbox://styles/mapbox/streets-v12',
     'center': [-2.834603077700183, 54.1973265832562],
     'zoom': 15})
-    
+   
+    # get location of trees using the tree type dropdown selection
     locations = app_tables.location.search(TreeType = self.drop_down_1.selected_value) #anvil.server.call('get_all_locations' )
     # self.hits_textbox.text = len(locations) 
     print(self.drop_down_1.selected_value)
+
+    # populate map with markers for tree4s for dropdown treetype  selection
     for location in locations:
        self.marker = mapboxgl.Marker({'color': 'brown', 'scale': '0.75', 'draggable': False})
        lat = location['Latitude']
@@ -44,7 +54,7 @@ class Form7(Form7Template):
        notes = location['Desc']
        treetype = location['TreeType']
        print('lng',lng, ' ' ,'lat',lat)
-
+       #create a marker popup text
        popuptext = ('Name:' + loc + '<br>' +
                     'Notes:' + notes + '<br>' +
                     'lat:' + str(lat) + '<br>' +   
@@ -56,28 +66,10 @@ class Form7(Form7Template):
        # print(self.dom.getElementById('button'))
        self.marker.setLngLat([lng,lat]).addTo(self.mapbox)
        popup = mapboxgl.Popup({ 'offset': 25, 'max-width': 1000}).setHTML(popuptext)
-
-      
+    
        self.marker.setPopup(popup)
        
-  # print('button', document.getElementById('View Full'))
-  # document.getElementById("myBtn")  #.addEventListener("click",self.click)
-  # # def click(self):
-  #   # alert('Syd')
-
-  # xdom.addEventListener('click',self.change_text)
-
-  # def change_text(self):
-  #  self.my_label.text='audio has finished playing'
-     
-    # iframe = jQuery("<iframe width='100%' height='800px'>").attr("src"," \
-    #  https://www.woodlandtrust.org.uk/trees-woods-and-wildlife/british-trees/a-z-of-british-trees/alder/") 
-    # iframe.appendTo(get_dom_node(self.column_panel_1))
-
-  def text_change(self, popuptext):
-    self.text_area_1.text = popuptext
-    alert(popuptext)
- 
+   # create a dragable blue marker for positioning a new tree
  
     self.marker = mapboxgl.Marker({'blue': '#0000FF', 'draggable': True})
     self.marker.setLngLat([-2.834603077700183, 54.1973265832562]).addTo(self.mapbox)
@@ -85,24 +77,9 @@ class Form7(Form7Template):
          '-2.834603077700183, 54.1973265832562')
     self.marker.on('dragend', self.onDragEnd)
     
-    self.marker.on('click', popuptext)
-    xdom=document.getElementsById("myBtn")
-    popupElem = popup.getElement(xdom)
-    popupElem.style.fontSize = "25px"
-    print(xdom[''])
-    xdom.addEventListener('click',self.text_change)
-    # self.marker.on('click', self.text_change)
-  def text_change(self):
-
+         
     
-    self.marker.on('dragend', self.onDragEnd)  
-
-   
-  
-  def logger(self, **event_args):
-     alert('syd')
-  
-  
+  # not ussed yet???
   def move_marker(self, result):
     # lnglat = result['result']['geometry']['coordinates']
     lnglat = self.marker.getLngLat();
@@ -110,9 +87,8 @@ class Form7(Form7Template):
          lnglat)
     self.marker.setPopup(popup)
     alert(lnglat)
-  def show_info(self,**event_args):
-      alert('Syd')
-    
+ 
+   # get lng lat of blue marker on dragging blue marker 
   def onDragEnd(self, dragend):
       xy = self.marker.getLngLat()
       alert(str(xy['lat']) + ' ' + str(xy['lng']))
@@ -122,7 +98,7 @@ class Form7(Form7Template):
       self.marker.setPopup(popup)
 
 
-
+   # add a new tree to database
   def add_location_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     lnglat = self.marker.getLngLat();
@@ -149,15 +125,17 @@ class Form7(Form7Template):
    
     pass
 
+   # repopulate map when treetype dropdown changed
   def drop_down_1_change(self, **event_args):
     """This method is called when an item is selected"""
+    # reset map to blank
     self.mapbox = mapboxgl.Map({
     'container': self.dom,
-    
-    # // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+        # // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     'style': 'mapbox://styles/mapbox/streets-v12',
     'center': [-2.834603077700183, 54.1973265832562],
     'zoom': 15})
+    
     self.marker = mapboxgl.Marker({'blue': '#0000FF', 'draggable': True})
     self.marker.setLngLat([-2.834603077700183, 54.1973265832562]).addTo(self.mapbox)
     popup = mapboxgl.Popup({ 'offset': 20 }).setText(
@@ -165,6 +143,7 @@ class Form7(Form7Template):
     self.marker.setPopup(popup)
     self.marker.on('dragend', self.onDragEnd)  
     
+    # get markers for tree type
     locations = app_tables.location.search(TreeType = self.drop_down_1.selected_value) #anvil.server.call('get_all_locations' )
     # self.hits_textbox.text = len(locations) 
     print(self.drop_down_1.selected_value)
@@ -176,7 +155,7 @@ class Form7(Form7Template):
        notes = location['Desc']
        treetype = location['TreeType']
        print('lng',lng, ' ' ,'lat',lat)
-
+      # get popup texts
        popuptext = ('Name:' + loc + '<br>' +
                     'Notes:' + notes + '<br>' +
                     'lat:' + str(lat) + '<br>' +   
@@ -184,13 +163,14 @@ class Form7(Form7Template):
                     'treetype ' + treetype + '<br>' +
                   '<button  id="myBtn">Hello </button>')
                    # '<button (click)="logger">View Full</button>')
-       self.marker.on('click', self.text_change)
+       
        # print(self.dom.getElementById('button'))
        self.marker.setLngLat([lng,lat]).addTo(self.mapbox)
        popup = mapboxgl.Popup({ 'offset': 25, 'max-width': 1000}).setHTML(popuptext)
-
-      
+     
        self.marker.setPopup(popup)
+      
+    # find Woodland trust webpage and doisplay in frame
     url = app_tables.trees.get(Name=self.drop_down_1.selected_value)
     self.column_panel_1.clear()
     # print('url',url['WoodlandTrust_link'],'dropdown',self.drop_down_1.selected_value )
@@ -199,15 +179,7 @@ class Form7(Form7Template):
     iframe.appendTo(get_dom_node(self.column_panel_1))
     pass
 
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.mapbox = mapboxgl.Map({
-    'container': self.dom,
-    
-    # // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    'style': 'mapbox://styles/mapbox/streets-v12',
-    'center': [-2.834603077700183, 54.1973265832562],
-    'zoom': 15})
+
 
 
 
